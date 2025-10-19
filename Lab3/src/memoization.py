f1dcc116a820fba5fd7336ab8ef4c52261839cdb
Dmitry <pilot7736gives@gmail.com>
2025-10-19 20:23:19 +0300

    Add code memoization

---
+++ Lab3/src/memoization.py
@@ -0,0 +1,30 @@
+import time
+from recursion import fibonacci
+
+# Мемоизированная версия
+def fibonacci_memo(n: int, memo=None) -> int:
+    if memo is None: # Инициализация словаря мемоизации при первом вызове O(1)
+        memo = {} # O(1)
+
+    if n == 0: # Базовые случаи
+        return 0 # O(1)
+    if n == 1: # O(1) 
+        return 1 # O(1)
+    
+    if n in memo: # Проверяем, не вычисляли ли мы уже это значение
+        return memo[n] # O(1)
+
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo) # Рекурсивный шаг с сохранением результата O(n)
+    return memo[n] # O(1)
+
+# Сравнение производительности
+if __name__ == "__main__":
+    n = 35
+
+    start = time.time()
+    print(f"Наивный Фибоначчи({n}) = {fibonacci(n)}")
+    print(f"Время работы (наивный): {time.time() - start:.4f} сек")
+
+    start = time.time()
+    print(f"Мемоизированный Фибоначчи({n}) = {fibonacci_memo(n)}")
+    print(f"Время работы (мемоизация): {time.time() - start:.4f} сек")
--- Lab3/src/recursion.py
+++ Lab3/src/recursion.py
@@ -1,6 +1,6 @@
 def factorial(n: int) -> int:
     """Вычисление факториала числа n рекурсивно."""
-    if n == 0 or n == 1:
+    if n == 0 or n == 1: # Базовые случаи
         return 1
     return n * factorial(n - 1)
 
@@ -10,9 +10,9 @@ def factorial(n: int) -> int:
 
 def fibonacci(n: int) -> int:
     """Рекурсивное вычисление n-го числа Фибоначчи."""
-    if n <= 1:
+    if n <= 1: # Базовые случаи 
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2) # Временная сложность: O(2^n)
 
 # Временная сложность: O(2^n)
 # Глубина рекурсии: O(n)
@@ -20,13 +20,13 @@ def fibonacci(n: int) -> int:
 
 def fast_power(a: float, n: int) -> float:
     """Быстрое возведение числа a в степень n (через деление степени на 2)."""
-    if n == 0:
+    if n == 0: # Базовые случаи 
         return 1
     if n % 2 == 0:
-        half = fast_power(a, n // 2)
+        half = fast_power(a, n // 2) # Рекурсивный вызов
         return half * half
-    else:
-        return a * fast_power(a, n - 1)
+    else: # n нечетное
+        return a * fast_power(a, n - 1) # Рекурсивный вызов
 
 # Временная сложность: O(log n)
 # Глубина рекурсии: O(log n)
